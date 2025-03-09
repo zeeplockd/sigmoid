bdaf7f78ae34b352d95f8504b931b7315081b052
fenziie <master.callum.burrows@gmail.com>
2025-03-09 08:46:38 +0000

    Added the sex command to SkibidiBot (:O) and made a setting to toggle the fuckoff command

--- src/main/java/me/zeeplockd/sigmoid/modules/SkibidiBot.java
+++ src/main/java/me/zeeplockd/sigmoid/modules/SkibidiBot.java
@@ -10,6 +10,10 @@ import net.minecraft.network.packet.s2c.common.DisconnectS2CPacket;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
 public class SkibidiBot extends Module {
     private final SettingGroup sgGeneral = this.settings.getDefaultGroup();
 
@@ -20,23 +24,37 @@ public class SkibidiBot extends Module {
         .build()
     );
 
+    private final Setting<Boolean> fuckOffEnabled = sgGeneral.add(new BoolSetting.Builder()
+        .name("fuckoff-enabled")
+        .description("Whether the 'fuckoff' command should be active or not.")
+        .defaultValue(true)
+        .build()
+    );
+
     public SkibidiBot() {
         super(Sigmoid.CATEGORY, "skibidi-bot", "Very funny chatbot.");
     }
 
     @EventHandler
     private void onReceiveMessage(ReceiveMessageEvent event) {
-        // Strip formatting from the incoming message
         String rawMessage = String.valueOf(event.getMessage().getString());  // Converts to plain text
 
-        // Now process the message without formatting
         if (rawMessage.contains(prefix + "help")) {
             ChatUtils.sendPlayerMsg("Hello! This is SkibidiBot, the chatbot built into Sigmoid, a meteor client addon made by zeeplockd. Run 'commands' to get a list of commands.");
         } else if (rawMessage.contains(prefix + "commands")) {
-            ChatUtils.sendPlayerMsg("Commands: commands, help, fuckoff");
+            ChatUtils.sendPlayerMsg("Commands: commands, help, fuckoff, sex");
         } else if (rawMessage.contains(prefix + "fuckoff")) {
-            ChatUtils.sendPlayerMsg("If you insist... Jeez.");
-            mc.player.networkHandler.onDisconnect(new DisconnectS2CPacket(Text.literal("%s[%sSkibidiBot%s] %sSomebody told you to fuck off.".formatted(Formatting.GRAY, Formatting.BLUE, Formatting.GRAY, Formatting.WHITE))));
+            if (fuckOffEnabled.get() == true) {
+                ChatUtils.sendPlayerMsg("If you insist... Jeez.");
+                mc.player.networkHandler.onDisconnect(new DisconnectS2CPacket(Text.literal("%s[%sSkibidiBot%s] %sSomebody told you to fuck off.".formatted(Formatting.GRAY, Formatting.BLUE, Formatting.GRAY, Formatting.WHITE))));
+            } else {
+                ChatUtils.sendPlayerMsg("Sorry! The fuckoff command is currently disabled. Beg me to enable it.");
+            }
+        } else if (rawMessage.contains(prefix + "sex")) {
+            List<String> strings = Arrays.asList("Nya~~", "Ohh~~", "Harder daddy~~", "I'm go- gonna.. AHH~~", "I love this~~");
+            Random random = new Random();
+            String randomString = strings.get(random.nextInt(strings.size()));
+            ChatUtils.sendPlayerMsg(randomString);
         }
     }
 }
